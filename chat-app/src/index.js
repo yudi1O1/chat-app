8e0d775613180b04abaa7ecb019cf2ac97dee238
uddeshya <uddeshyar1.asnkvb@gmail.com>
2024-09-02 08:49:27 +0530

    msg

--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import "./index.css"
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 
 
@@ -9,6 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
     <App />
+    <SpeedInsights/>
   </>
 );
 
